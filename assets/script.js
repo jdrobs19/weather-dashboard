5f8e073554cd6e40d0d226355b85b1a05008b531
Jordan Roberts <jdrobs19@gmail.com>
2020-09-03 14:42:49 -0600

    populating current forcast

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,11 @@ let userFormEl = document.querySelector("#user-form");
 let cityInputEl = document.querySelector("#city");
 let weatherContainerEl = document.querySelector("#weather-container");
 let citySearchTerm = document.querySelector("#city-searched")
+let fiveDay = document.querySelector("#five-day");
 
 let getForcast = function(city){
 
-    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+    let apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
 
     fetch(apiUrl)
     .then(response => response.json())
@@ -14,10 +15,54 @@ let getForcast = function(city){
             
 };
 
-let displayForcast = function(data){
+let displayForcast = function(data, searchTerm){
     console.log(data);
-    let temp = data.main.temp;
-    console.log(temp);
+    let date = moment().format('L');;
+    
+    let temp = Math.floor(data.list[0].main.temp);
+    let weather = data.list[0].weather[0].main;
+    let humidity = data.list[0].main.humidity;
+    let wind = Math.floor(data.list[0].wind.speed);
+
+    let currentDateEl = document.createElement('h5');
+    currentDateEl.textContent = date;
+
+    let currentTempEl = document.createElement('span');
+    currentTempEl.innerHTML = "<h6>Temperature: </h6>" + temp + "℉";
+
+    let currentWeatherEl = document.createElement('span');
+    currentWeatherEl.innerHTML = "<h6>Currently: </h6>" + weather;
+
+    let currentHumidityEl = document.createElement('span');
+    currentHumidityEl.innerHTML = "<h6>Humidity: </h6>" + humidity + "%";
+
+    let currentWindEl = document.createElement('span');
+    currentWindEl.innerHTML = "<h6>Wind Speed: </h6>" + wind + " MPH";
+
+    
+    weatherContainerEl.appendChild(currentDateEl);
+    weatherContainerEl.appendChild(currentTempEl);
+    weatherContainerEl.appendChild(currentWeatherEl);
+    weatherContainerEl.appendChild(currentHumidityEl);
+    weatherContainerEl.appendChild(currentWindEl);
+
+    let forcastObject ={
+        forcastDate: moment([i]),
+        forcastTemp: Math.floor(data.list[i].main.temp),
+        forcastWeather: data.list[i].weather[i].main,
+        forcastHumidity: data.list[i].main.humidity,
+    }
+
+    let fiveDayForcast = function(){
+        for(i = 1; i < 5; i++){
+
+    }
+};
+
+    fiveDayEl = document.createElement('span');
+    fiveDayEl.innerHTML = 
+
+    fiveDay.appendChild(fiveDayEl);
 };
 
 let formSubmitHandler = function(event){
