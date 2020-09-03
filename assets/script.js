1335df1728ae8850ca8102e3d73cf3eb69b11941
Jordan Roberts <jdrobs19@gmail.com>
2020-09-03 09:43:57 -0600

    testing temp data

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,28 @@
 let apiKey = "728c0e97216666478e240f7049f6a80e"
 let userFormEl = document.querySelector("#user-form");
 let cityInputEl = document.querySelector("#city");
+let weatherContainerEl = document.querySelector("#weather-container");
+let citySearchTerm = document.querySelector("#city-searched")
 
 let getForcast = function(city){
 
-    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 
     fetch(apiUrl)
     .then(response => response.json())
-    .then(data=> console.log(data));
+    .then(data=> displayForcast(data));
             
+};
+
+let displayForcast = function(data){
+    console.log(data);
+    let temp = data.main.temp;
+    console.log(temp);
 };
 
 let formSubmitHandler = function(event){
     event.preventDefault();
+
     let name = cityInputEl.value.trim();
 
     if(name){
@@ -24,6 +33,8 @@ let formSubmitHandler = function(event){
     else{
         alert("Please enter a city.");
     }
+
+
 };
 
-userFormEl.addEventListener("sumbit", formSubmitHandler);
+userFormEl.addEventListener("submit", formSubmitHandler);
