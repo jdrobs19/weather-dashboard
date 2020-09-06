dbcfc788c9d72fb32521a0919c9c4e6b886d3847
Jordan Roberts <jdrobs19@gmail.com>
2020-09-06 12:08:31 -0600

    displaying forcast information and saving user input to local

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,12 @@ let apiKey = "728c0e97216666478e240f7049f6a80e"
 let userFormEl = document.querySelector("#user-form");
 let cityInputEl = document.querySelector("#city");
 let weatherContainerEl = document.querySelector("#weather-container");
+let displayCity = document.querySelector("#display-city");
 let citySearchTerm = document.querySelector("#city-searched")
 let fiveDay = document.querySelector("#five-day-cards");
+let fiveDayHeader = document.querySelector("#five-day-header");
+let searchHistory = document.querySelector("#search-history");
+let searched = [];
 
 let getForcast = function (city) {
 
@@ -16,11 +20,12 @@ let getForcast = function (city) {
 };
 
 let displayForcast = function (data, searchTerm) {
+    weatherContainerEl.textContent = '';
+    fiveDay.textContent = '';
     console.log(data);
-    let date = moment().format('L');;
 
-    
-
+    let date = moment().format('L');
+    let city = data.city.name; 
     let temp = Math.floor(data.list[0].main.temp);
     let weather = data.list[0].weather[0].icon;
     let humidity = data.list[0].main.humidity;
@@ -43,31 +48,31 @@ let displayForcast = function (data, searchTerm) {
     let currentWindEl = document.createElement('span');
     currentWindEl.innerHTML = "<h6>Wind Speed: </h6>" + wind + " MPH";
 
-
+    citySearchTerm.textContent = city;
     weatherContainerEl.appendChild(currentDateEl);
     currentDateEl.appendChild(currentWeatherEl);
     weatherContainerEl.appendChild(currentTempEl);
     weatherContainerEl.appendChild(currentHumidityEl);
     weatherContainerEl.appendChild(currentWindEl);
 
-        for(let i = 2; i < 40; i+=8){
-            let forcastObject = {
-                forcastDate: moment(data.list[i].dt_txt).format('L'),
-                forcastTemp: Math.floor(data.list[i].main.temp) + "℉",
-                forcastWeather: data.list[i].weather[0].icon,
-                forcastHumidity: data.list[i].main.humidity + "%",
-            };
+    for (let i = 2; i < 40; i += 8) {
+        let forcastObject = {
+            forcastDate: moment(data.list[i].dt_txt).format('L'),
+            forcastTemp: Math.floor(data.list[i].main.temp) + "℉",
+            forcastWeather: data.list[i].weather[0].icon,
+            forcastHumidity: data.list[i].main.humidity + "%",
+        };
 
-            console.log(forcastObject);
-            let fiveDayEl = document.createElement('div');
-    fiveDayEl.setAttribute("class", "card-body");
-    fiveDayEl.textContent = forcastObject;
+        console.log(forcastObject);
+        let fiveDayEl = document.createElement('div');
+        fiveDayEl.classList = "card bg-primary";
+        fiveDayEl.innerHTML = `${forcastObject.forcastDate}<br><img src=https://openweathermap.org/img/w/${forcastObject.forcastWeather}.png width='50' height='50'><br><p>Temp: </p>${forcastObject.forcastTemp}<p>Humidity: </p>${forcastObject.forcastHumidity}`;
 
-    fiveDay.appendChild(fiveDayEl);
+        fiveDayHeader.setAttribute("class", "card-header");
+        fiveDayHeader.textContent = "5 Day Forcast";
+
+        fiveDay.appendChild(fiveDayEl);
     };
-    
-    
-
 };
 
 let formSubmitHandler = function (event) {
@@ -83,11 +88,31 @@ let formSubmitHandler = function (event) {
     else {
         alert("Please enter a city.");
     }
-    
-    // let searchArr = [];
-    // searchArr.push(name);
 
-    // localStorage.setItem('cityName', JSON.stringify(searchArr));
+    searched.push(name);
+    saveSearch();
+};
+
+let saveSearch = function(){
+    localStorage.setItem("city", JSON.stringify(searched));
+};
+
+ let pageLoad = function(){
+    let savedSearch= localStorage.getItem('cityName');
+    if(!savedSearch){
+        return false;
+    }
+
+    savedSearch = JSON.parse(savedSearch);
+
+    for (let j = 0; j < savedSearch.length; i++){
+        
+    }
+};
+
+let recentClickHandler = function(event){
+    let recentCity = event.target.getAttribute("data-search");
+    
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
